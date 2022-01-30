e401a1c1c5a7c045d1e3e2df9812bdfbac9aa89f
NicholasHorta <nickdeveloper.js@gmil.com>
2022-01-30 10:21:47 +0000

    Google Sign in complete

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { GoogleSignInDirective } from './google-sign-in.directive';
 
 @NgModule({
   declarations: [
-    LoginPageComponent
+    LoginPageComponent,
+    GoogleSignInDirective
   ],
   imports: [
     CommonModule,
